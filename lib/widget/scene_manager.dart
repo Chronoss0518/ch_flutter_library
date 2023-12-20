7fcd4340f09f8f6ceb9eab82aa69ac9b7d57e15a
Chronoss0518 <sinyusuke5000@gmail.com>
2023-12-20 15:08:44 +0900

    保持するデータをすべてSceneManagerClassが保持するように修正

--- lib/widget/scene_manager.dart
+++ lib/widget/scene_manager.dart
@@ -78,118 +78,10 @@ abstract class BaseScene extends StatelessWidget {
 class SceneManager extends StatefulWidget {
   //Constructor Destructor//
   SceneManager(BaseScene startScene, {super.key}) {
-    _startScene = startScene;
+    _nextScene = startScene;
   }
 
   SceneManagerState state = SceneManagerState();
-  BaseScene? _startScene = null;
-  //OverrideFunction//
-  @override
-  State<SceneManager> createState() => state;
-}
-
-class SceneManagerState extends State<SceneManager> {
-  //Initialize Release//
-  void _init() {
-    if (_isInitFlg) return;
-    _timerStart();
-    _setScene(widget._startScene!);
-    widget._startScene = null;
-    _isInitFlg = true;
-  }
-
-  //SetFunctions//
-  void _setAppBar(AppBar? appBar) {
-    _appBar = appBar;
-  }
-
-  void _setBottomNavigationBar(BottomNavigationBar? bottomNavigationBar) {
-    _bottomNavigationBar = bottomNavigationBar;
-  }
-
-  void _setScene(BaseScene scene) {
-    if (scene._state != null) return;
-    _nextScene = scene;
-  }
-
-  void _setFps(int fps) {
-    if (fps <= 0 || fps >= 1000) return;
-    _fps = fps;
-    _timer.cancel();
-    _timerStart();
-  }
-
-  void _setSendData(SendBeforeSceneData sendData) {
-    _sendData = sendData;
-  }
-
-//ClearFunctions//
-
-  void _clearAppBar() {
-    _appBar = null;
-  }
-
-  void _clearBottomNavigationBar() {
-    _bottomNavigationBar = null;
-  }
-
-//OtherFunctions//
-
-  void _repaint(void Function() func) {
-    if (!_isInitFlg) return;
-    setState(func);
-  }
-
-  void _changeScene() {
-    if (_nextScene == null) return;
-    BaseScene next = _nextScene!;
-    if (next._state != null) {
-      _nextScene = null;
-      return;
-    }
-    _scene?.release();
-    _scene?._state = null;
-
-    _scene = next;
-    _scene?._state = this;
-    _scene?.init(sendData: _sendData);
-
-    _nextScene = null;
-    _sendData = null;
-  }
-
-  void _timerStart() {
-    _timer =
-        Timer.periodic(Duration(milliseconds: (1000 / _fps).toInt()), (timer) {
-      _scene?.update();
-      _scene?.move();
-
-      _changeScene();
-    });
-  }
-
-  //OverrideFunction//
-  @override
-  Widget build(BuildContext context) {
-    _init();
-    _scene?.drawBegin(context);
-    Widget body = _scene ?? Container();
-    return Scaffold(
-      appBar: _appBar,
-      body: body,
-      bottomNavigationBar: _bottomNavigationBar,
-    );
-  }
-
-  @override
-  void dispose() {
-    super.dispose();
-    if (!_isInitFlg) return;
-    _scene = null;
-    _isInitFlg = false;
-    _timer.cancel();
-  }
-
   //Values//
   AppBar? _appBar = null;
   BaseScene? _scene = null;
@@ -200,4 +92,109 @@ class SceneManagerState extends State<SceneManager> {
   late Timer _timer;
   int _fps = 60;
   bool _isInitFlg = false;
+  //OverrideFunction//
+  @override
+  State<SceneManager> createState() => state;
+}
+
+class SceneManagerState extends State<SceneManager> {
+  //Initialize Release//
+  void _init() {
+    if (widget._isInitFlg) return;
+    _timerStart();
+    _changeScene();
+    widget._isInitFlg = true;
+  }
+
+  //SetFunctions//
+  void _setAppBar(AppBar? appBar) {
+    widget._appBar = appBar;
+  }
+
+  void _setBottomNavigationBar(BottomNavigationBar? bottomNavigationBar) {
+    widget._bottomNavigationBar = bottomNavigationBar;
+  }
+
+  void _setScene(BaseScene scene) {
+    if (scene._state != null) return;
+    widget._nextScene = scene;
+  }
+
+  void _setFps(int fps) {
+    if (fps <= 0 || fps >= 1000) return;
+    widget._fps = fps;
+    widget._timer.cancel();
+    _timerStart();
+  }
+
+  void _setSendData(SendBeforeSceneData sendData) {
+    widget._sendData = sendData;
+  }
+
+//ClearFunctions//
+
+  void _clearAppBar() {
+    widget._appBar = null;
+  }
+
+  void _clearBottomNavigationBar() {
+    widget._bottomNavigationBar = null;
+  }
+
+//OtherFunctions//
+
+  void _repaint(void Function() func) {
+    if (!widget._isInitFlg) return;
+    setState(func);
+  }
+
+  void _changeScene() {
+    if (widget._nextScene == null) return;
+    BaseScene next = widget._nextScene!;
+    if (next._state != null) {
+      widget._nextScene = null;
+      return;
+    }
+    widget._scene?.release();
+    widget._scene?._state = null;
+
+    widget._scene = next;
+    widget._scene?._state = this;
+    widget._scene?.init(sendData: widget._sendData);
+
+    widget._nextScene = null;
+    widget._sendData = null;
+  }
+
+  void _timerStart() {
+    widget._timer = Timer.periodic(
+        Duration(milliseconds: (1000 / widget._fps).toInt()), (timer) {
+      widget._scene?.update();
+      widget._scene?.move();
+
+      _changeScene();
+    });
+  }
+
+  //OverrideFunction//
+  @override
+  Widget build(BuildContext context) {
+    _init();
+    widget._scene?.drawBegin(context);
+    Widget body = widget._scene ?? Container();
+    return Scaffold(
+      appBar: widget._appBar,
+      body: body,
+      bottomNavigationBar: widget._bottomNavigationBar,
+    );
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    if (!widget._isInitFlg) return;
+    widget._scene = null;
+    widget._isInitFlg = false;
+    widget._timer.cancel();
+  }
 }
