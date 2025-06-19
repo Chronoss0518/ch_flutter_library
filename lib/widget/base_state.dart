8ba434231ffd2d161c28980e7ae8e434cdf18a5f
Chronoss0518 <sinyusuke5000@gmail.com>
2025-06-19 16:40:17 +0900

    BaseStateクラスのupdateとmoveをSceneManager側の機能に合わせる形で修正

--- lib/widget/base_state.dart
+++ lib/widget/base_state.dart
@@ -15,17 +15,9 @@ abstract class BaseState<T extends StatefulWidget> extends State<T> {
   @protected
   void init() {}
 
-  @protected
-  void update() {}
-
-  @protected
-  void move() {}
-
   @mustCallSuper
   @protected
-  void release() {
-    _timer.cancel();
-  }
+  void release() {}
 
   @protected
   void changeEvent() {}
@@ -39,7 +31,6 @@ abstract class BaseState<T extends StatefulWidget> extends State<T> {
   void initState() {
     super.initState();
     init();
-    _timerStart();
   }
 
   @mustCallSuper
@@ -50,22 +41,15 @@ abstract class BaseState<T extends StatefulWidget> extends State<T> {
     super.dispose();
   }
 
-  void setFps(int fps) {
-    if (fps <= 0 || fps >= 1000) return;
-
-    _timer.cancel();
-    _timerStart();
-  }
-
-  void nextScene(StatefulWidget _nextScene, String sceneName) {
+  void nextScene(StatefulWidget nextScene, String sceneName,{BuildContext? inContext}) {
     changeEvent();
     release();
-
-    Navigator.of(context)
+    BuildContext tmpContext = inContext ?? context;
+    Navigator.of(tmpContext)
         .push(
       MaterialPageRoute<void>(
-        builder: (BuildContext context) {
-          return _nextScene;
+        builder: (BuildContext tmpContext) {
+          return nextScene;
         },
         settings: RouteSettings(name: sceneName),
       ),
@@ -76,29 +60,76 @@ abstract class BaseState<T extends StatefulWidget> extends State<T> {
     });
   }
 
-  void backScene() {
+  void backScene({BuildContext? inContext}) {
     changeEvent();
     release();
-    Navigator.of(context).pop();
+    BuildContext tmpContext = inContext ?? context;
+    Navigator.of(tmpContext).pop();
   }
 
-  void backSceneToFirst() {
-    Navigator.of(context).popUntil((route) {
+  void backSceneToFirst({BuildContext? inContext}) {
+    BuildContext tmpContext = inContext ?? context;
+    Navigator.of(tmpContext).popUntil((route) {
       return route.isFirst;
     });
   }
 
-  void backSceneToStateName(String sceneName) {
-    Navigator.of(context).popUntil((route) {
+  void backSceneToStateName(String sceneName,{BuildContext? inContext}) {
+    BuildContext tmpContext = inContext ?? context;
+    Navigator.of(tmpContext).popUntil((route) {
       return route.settings.name == sceneName;
     });
   }
+}
+
+/*
+このクラスはStateの代わりに利用します
+このクラスは次のメソッドをoverrideして利用します。
+- init()initState、またはpopにより戻ってきた際に走ります
+- update()1フレーム毎に走ります。setFpsにより途中でfps値を変更できます
+- release()このStatefulWidgetが破棄されたとき(disposeが走る時)に走ります
+- changeEvemt()
+*/
+abstract class BaseStateEx<T extends StatefulWidget> extends BaseState<T> {
+  
+  void Function()? update;
+
+  void Function()? move;
+
+  @mustCallSuper
+  @protected
+  @override
+  void release() {
+    super.release();
+    _timer.cancel();
+  }
+
+  @mustCallSuper
+  @protected
+  @override
+  void initState() {
+    super.initState();
+    init();
+    _timerStart();
+  }
+
+  void setFps(int fps) {
+    if (fps <= 0 || fps >= 1000) return;
+    _fps = fps;
+    _timer.cancel();
+    _timerStart();
+  }
 
   void _timerStart() {
+    
+    if (_fps <= 0 || _fps >= 1000) return;
+    if( update == null &&
+    move == null){return;}
+    
     _timer =
-        Timer.periodic(Duration(milliseconds: (1000 / _fps).toInt()), (timer) {
-      update();
-      move();
+        Timer.periodic(Duration(milliseconds: (1000.0 / _fps.toDouble()).toInt()), (timer) {
+      if(update != null)update!();
+      if(move != null)move!();
     });
   }
 
